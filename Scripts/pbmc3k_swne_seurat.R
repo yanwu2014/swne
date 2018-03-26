4194db9b4318cbd8950f9ba4cdac4cc6dfd6a9fb
Yan Wu <yauwning@gmail.com>
2018-03-26 16:41:02 -0700

    Add R markdown walkthrough for using SWNE with Seurat

--- Scripts/pbmc3k_swne_seurat.R
+++ Scripts/pbmc3k_swne_seurat.R
@@ -4,10 +4,8 @@ library(swne)
 ## Read in Seurat object
 se.obj <- readRDS("pbmc3k_seurat.Robj")
 
-## Pull out counts matrix
+## Pull out counts matrix, filter and trim data
 counts <- as(se.obj@raw.data, "dgCMatrix")[,se.obj@cell.names]
-
-## Filter raw counts
 counts <- FilterData(counts, min.samples.frac = 0.0025, min.nonzero.features = 200, trim = 0.001)
 dim(counts)
 
@@ -18,19 +16,18 @@ norm.counts <- ScaleCounts(counts, batch = NULL, method = "log", adj.var = T)
 var.genes <- intersect(se.obj@var.genes, rownames(counts));
 length(var.genes)
 
-## Pull out cell clusters and defined by Seurat
+## Pull out cell clusters as defined by Seurat
 cell.clusters <- se.obj@ident; names(cell.clusters) <- se.obj@cell.names;
 levels(cell.clusters)
 
 ## Unguided NMF
 loss <- "mse" ## Loss function
-k.range <- seq(1,10,1) ## Range of factors to iterate over
 n.cores <- 16 ## Number of cores to use
-color.seed <- 32566 ## Set seed for cluster colors
 
-# ## Identify optimal number of factors
-# n.comp.res <- FindNumFactors(norm.counts[var.genes,], k.range = k.range, n.cores = n.cores, do.plot = T, loss = loss)
-# n.comp.res$k
+## Identify optimal number of factors
+k.range <- seq(1,10,1) ## Range of factors to iterate over
+k.res <- FindNumFactors(norm.counts[var.genes,], k.range = k.range, n.cores = n.cores, do.plot = T, loss = loss)
+k.res$k
 
 ## Run NMF
 k <- 8
@@ -38,30 +35,34 @@ nmf.res <- RunNMF(norm.counts[var.genes,], k = k, alpha = 0, init = "ica", n.cor
 nmf.scores <- nmf.res$H
 
 ## Compute snn or use pre-computed snn from Seurat
-# pc.scores <- t(GetCellEmbeddings(se.obj, reduction.type = "pca", dims.use = 1:8))
+# pc.scores <- t(GetCellEmbeddings(se.obj, reduction.type = "pca", dims.use = 1:k))
 # snn <- CalcSNN(pc.scores)
 snn <- se.obj@snn
 
 ## Run SWNE embedding
-swne.embedding <- EmbedSWNE(nmf.scores, snn, alpha.exp = 1.0, snn.exp = 1,
-                            n_pull = 4, dist.use = "IC")
+alpha.exp <- 1.5 # Increase this to move the cells closer to the factors. Values > 2 start to distort the data.
+snn.exp <- 0.5 # Lower this to move similar cells closer to each other
+n_pull <- 4 # The number of factors pulling on each cell. Must be at least 3.
+swne.embedding <- EmbedSWNE(nmf.scores, snn, alpha.exp = alpha.exp, snn.exp = snn.exp,
+                            n_pull = n_pull, dist.use = "IC")
 
 ## Plot SWNE
+color.seed <- 32566 ## Set seed for cluster colors
 PlotSWNE(swne.embedding, alpha.plot = 0.4, sample.groups = cell.clusters, do.label = T,
-         label.size = 4, pt.size = 1.5, show.legend = T, seed = color.seed)
+         label.size = 3.5, pt.size = 1.25, show.legend = T, seed = color.seed)
 
 ## Plot tSNE for comparison
 tsne.scores <- GetCellEmbeddings(se.obj, reduction.type = "tsne")
-PlotDims(tsne.scores, sample.groups = cell.clusters, pt.size = 1, label.size = 3, alpha = 0.5,
-         show.legend = F, seed = color.seed)
+PlotDims(tsne.scores, sample.groups = cell.clusters, pt.size = 1, label.size = 3.5, alpha = 0.4,
+         show.legend = F, seed = color.seed, show.axes = F)
 
 ## Annotate factors with marker genes
-gene.factor.cors <- FactorAssociation(norm.counts, nmf.scores, n.cores = n.cores, metric = "pearson")
-top.factor.genes.df <- SummarizeAssocFeatures(gene.factor.cors, features.return = 5)
+gene.loadings <- nmf.res$W
+top.factor.genes.df <- SummarizeAssocFeatures(gene.loadings, features.return = 2)
 
 ## Plot heatmap
-gene.factor.cors.plot <- gene.factor.cors[unique(top.factor.genes.df$feature),]
-ggHeat(gene.factor.cors.plot, clustering = "col")
+gene.loadings.heat <- gene.loadings[unique(top.factor.genes.df$feature),]
+ggHeat(gene.loadings.heat, clustering = "col")
 
 ## Rename some factors
 # swne.embedding <- RenameFactors(swne.embedding, name.mapping = c("factor_1" = "B-Cell Signaling"))
