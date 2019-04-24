395311a4fd96f2f26fd2fac4c1cc56ace8039123
Yan Wu <yauwning@gmail.com>
2019-04-24 14:45:21 -0700

    Add TF binding site embedding for chromatin data

--- R/chromatin.R
+++ R/chromatin.R
@@ -40,3 +40,46 @@ EmbedPromoters <- function(swne.embedding, cisTopicObject, genes.embed, peaks.us
 
   swne.emb
 }
+
+
+
+#' Embeds genes based on TF binding site accessibility relative to topic coordinates
+#'
+#' @param swne.embedding SWNE embedding computed using RunSWNE
+#' @param cisTopicObject cisTopic object
+#' @param motif_ix_mat A binary matrix of peaks x TF motifs (can be generated with ChromVar)
+#' @param genes.embed Genes to embed
+#' @param peaks.use Subset of peaks to use
+#' @param alpha.exp Increasing alpha.exp increases how much the factors "pull" the features
+#' @param n_pull Number of factors pulling on each feature. Must be >= 3
+#' @param scale.cols Whether or not to scale the input columns to 0 - 1
+#' @param overwrite Whether or not to overwrite any existing feature embedding
+#'
+#' @return swne.embedding with feature coordinates (feature.coords)
+#'
+#' @export
+#'
+EmbedTFBS <- function(swne.embedding, cisTopicObject, motif_ix_mat, genes.embed, peaks.use = NULL,
+                      alpha.exp = 1, n_pull = 3, scale.cols = T, overwrite = T) {
+  if (!requireNamespace("cisTopic", quietly = T)) {
+    stop("cisTopic needed for this function to work. Please install it.",
+         call. = F)
+  }
+
+  topic.regions <- as.matrix(cisTopicObject@region.data)
+  if (is.null(peaks.use)) peaks.use <- rownames(topic.regions)
+
+  topic.regions <- topic.regions[peaks.use, grepl("Scores", colnames(topic.regions))]
+  topic.regions <- apply(topic.regions, 2, as.numeric)
+  rownames(topic.regions) <- rownames(cisTopicObject@region.data)
+
+  if (!all(genes.embed %in% colnames(motif_ix_mat))) {
+    print("TFs missing from motif matrix")
+    print(paste(genes.embed[!genes.embed %in% colnames(motif_ix_mat)], collapse = ", ", sep = ", "))
+  }
+  genes.embed <- genes.embed[genes.embed %in% colnames(motif_ix_mat)]
+  motif_topics <- t(as.matrix(motif_ix_mat[,genes.embed])) %*% topic.regions[rownames(motif_ix_mat),]
+  rownames(motif_topics) <- paste0(rownames(motif_topics), "-tfbs")
+  EmbedFeatures(swne.embedding, motif_topics, alpha.exp = alpha.exp, n_pull = n_pull,
+                scale.cols = scale.cols, overwrite = overwrite)
+}
