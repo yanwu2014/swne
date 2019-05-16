258c079d30a14081332c62bf5ac574d5e4543b9f
Yan Wu <yauwning@gmail.com>
2019-05-16 13:06:22 -0700

    Update EmbedTFBS

--- R/chromatin.R
+++ R/chromatin.R
@@ -78,6 +78,7 @@ EmbedTFBS <- function(swne.embedding, cisTopicObject, motif_ix_mat = NULL, dev.m
   if(!is.null(dev.mat)) {
     topic.emb <- modelMatSelection(cisTopicObject, target = "cell", method = "Probability")
     tf.topic.cor <- FactorAssociation(dev.mat[genes.embed,], topic.emb, metric = "pearson")
+    rownames(tf.topic.cor ) <- paste0(rownames(tf.topic.cor), "-tfbs")
     swne.embedding <- EmbedFeatures(swne.embedding, tf.topic.cor, alpha.exp = alpha.exp, n_pull = n_pull,
                                     scale.cols = scale.cols, overwrite = overwrite)
 
